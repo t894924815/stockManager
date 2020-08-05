025825f4dd6423d3e22670876b06a212e100eb5b
t894924815 <chunriyeqiukong@gmail.com>
2020-08-05 17:11:55 +0800

    remove shell

--- stockManager/run_server.sh
+++
@@ -1,1 +0,0 @@
-nohup python3.8 manage.py runsslserver --certificate /etc/letsencrypt/live/api.stock.iakira.moe/fullchain.pem --key /etc/letsencrypt/live/api.stock.iakira.moe/privkey.pem > test.log 2>&1 &
